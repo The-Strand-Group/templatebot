a816617a2c469ad869e0463e99644c954c6bacec
Sword <quinn@bigbrother.group>
2020-02-09 18:26:43 -0600

    Whoops, that shouldn't have been committed

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
 
   // This should be your ID and your ID only. It will allow access to higher level commands that users should not be able to access
-  ownerID: '248540313059196928',
+  ownerID: 'Your ID',
 
   // This is your Discord bot's token. DO NOT SHARE IT WITH  A N Y O N E!
-  token: 'NjczNjY3ODIwMTIyNTM3OTk1.XjdYFQ.NWHItWrUUX7DiUrsDcqlVYpNEVc',
+  token: 'Bottoken',
 
   settings: {
     prefix: '$',
